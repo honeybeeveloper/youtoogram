5bb8d5beb444df034f7ba19a98efc97db92b3f46
honeybeeveloper <honeybeeveloper@naver.com>
2022-09-02 11:31:19 +0900

    언팔로우 API 추가

--- youtoogram/api/follow.py
+++ youtoogram/api/follow.py
@@ -5,7 +5,7 @@ from flask import jsonify, request
 from youtoogram.api.route import Route
 from youtoogram.common.exception import BadRequest, UserNotFound
 from youtoogram.feature.follow import Follow
-from youtoogram.feature.sign_up import SignUp
+from youtoogram.feature.users import Users
 
 
 class FollowAPI(object):
@@ -16,14 +16,29 @@ class FollowAPI(object):
         # check user-id == follow-id
         if data['user_id'] == data['follow_id']:
             raise BadRequest('can not follow yourself.')
-        # check the follow-id duplication
-        if not SignUp.is_exists_user_id(data['follow_id']):
+        # check the follow-id existence
+        if not Users.is_exists_user_id(data['follow_id']):
             raise UserNotFound('follow-id is not found!')
 
         Follow.create(data=data, now=now)
         return jsonify(f'{data["user_id"]} follow {data["follow_id"]} successfully!')
 
+    @staticmethod
+    def unfollow():
+        data = request.json
+        # check user-id == follow-id
+        if data['user_id'] == data['follow_id']:
+            raise BadRequest('can not unfollow yourself.')
+
+        # check the follow-id existence
+        if not Follow.is_exists_follow_id(data['user_id'], data['follow_id']):
+            raise UserNotFound('follow-id is not found!')
+
+        Follow.delete(data['user_id'], data['follow_id'])
+        return jsonify(f'{data["user_id"]} unfollow {data["follow_id"]} successfully!')
+
 
 routes = [
-    Route(uri='/follow/addition', view_func=FollowAPI.follow, methods=['POST'])
+    Route(uri='/follow', view_func=FollowAPI.follow, methods=['POST']),
+    Route(uri='/follow', view_func=FollowAPI.unfollow, methods=['DELETE'])
 ]
